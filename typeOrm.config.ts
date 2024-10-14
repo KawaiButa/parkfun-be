ba7a2e5aedddc13c3cfeb13ad5aa5c6698b76b5a
Francis-GOS <francis.nguyen.gos@gmail.com>
2024-10-14 10:14:44 +0700

    <FIX>: add parseInt to typeOrm config

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -8,7 +8,7 @@ const configService = new ConfigService();
 export default new DataSource({
   type: "postgres",
   host: configService.get("DB_HOST"),
-  port: configService.get("DB_PORT"),
+  port: parseInt(configService.get("DB_PORT")),
   username: configService.get("DB_USER"),
   password: configService.get("DB_PASSWORD"),
   database: configService.get("DB_NAME"),
